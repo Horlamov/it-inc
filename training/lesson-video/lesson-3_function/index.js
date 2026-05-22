4bc41456a465fe7cc2a7176fc24fbea195aa0964
Denis H <XoMA32@yandex.ru>
2026-05-22 23:23:28 +0300

    create function

--- training/lesson-video/lesson-3_function/index.js
+++ training/lesson-video/lesson-3_function/index.js
@@ -2,11 +2,18 @@ let budget = 100;
 let buildingName = "house";
 let buildingCost = 70;
 
-if(buildingCost <= budget){
-    console.log(`${buildingName} completed!`);
-    budget -= buildingCost;
-} else {
-    console.log(`For ${buildingName} you don't have enough budget`);
+
+
+function constructBuilding(name, cost) {
+    if (cost <= budget) {
+        console.log(`${name} completed!`);
+        budget -= cost;
+    } else {
+        console.log(`For ${name} you don't have enough budget`);
+    }
+
+    console.log(`The rest is ${budget} after building house`)
+
 }
 
-console.log(`The rest is ${budget} after building house`)
+constructBuilding("house", 70);
