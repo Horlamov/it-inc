5b3dcb6c827a7adecc44a8a44d6c71e23b974b5c
Denis H <XoMA32@yandex.ru>
2026-05-24 09:18:49 +0300

    rename a, b

--- js-from-scratch-hw-js-main/lesson-03/01.js
+++ js-from-scratch-hw-js-main/lesson-03/01.js
@@ -1,7 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+// let a;
+// let b;
 
-function calculateAverage(x, y) {
-    sum = (x + y)/2;
+function calculateAverage(a, b) {
+    sum = (a + b)/2;
     console.log(sum)
 }
 calculateAverage(2,4)
