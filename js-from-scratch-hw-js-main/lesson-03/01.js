847f742b026d4b6e4d8f13ec9dc3af9c2493f0c2
Denis H <XoMA32@yandex.ru>
2026-05-24 09:44:25 +0300

    refactor function

--- js-from-scratch-hw-js-main/lesson-03/01.js
+++ js-from-scratch-hw-js-main/lesson-03/01.js
@@ -1,10 +1,10 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
-let a;
-let b;
+
 let expected;
 
 
 function calculateAverage(a, b) {
-   return expected = (a + b)/2;
+    return (a + b)/2;
 }
-calculateAverage(10,20)
+expected = calculateAverage(10,20);
+console.log(expected);
