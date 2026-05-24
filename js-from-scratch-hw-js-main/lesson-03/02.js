90b9798d34bc613ef598a4a7b21be9f5d69c5de4
Denis H <XoMA32@yandex.ru>
2026-05-24 10:06:38 +0300

    add lesson-3_02

--- js-from-scratch-hw-js-main/lesson-03/02.js
+++ js-from-scratch-hw-js-main/lesson-03/02.js
@@ -1,4 +1,13 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
+function isEven(a) {
+    if (a % 2 === 0) {
+        isEven = true;
+        console.log("Число четное")
+    } else {
+        isEven = false;
+        console.log("Число нечетное")
 
-function isEven() {}
+    }
+return isEven;
+}
