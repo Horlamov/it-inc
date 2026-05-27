eb3dfc504f9d7b41cfd98aa3b4818ef2205cac84
Denis H <XoMA32@yandex.ru>
2026-05-27 22:27:30 +0300

    check

--- js-from-scratch-hw-js-main/lesson-02/01.js
+++ js-from-scratch-hw-js-main/lesson-02/01.js
@@ -16,7 +16,7 @@ const hasTemporaryPass = false
 let isAccess
 
 // your code
-if (isAdmin === true || isVerifiedUser === true && hasSpecialPermission === true || hasTemporaryPass === true && hasTemporaryPass === true) {
+if (isAdmin === true || isVerifiedUser === true && hasSpecialPermission === true || hasTemporaryPass === true) {
     isAccess = true
     console.log('Доступ разрешен')
 } else {
