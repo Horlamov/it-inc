9c19c91cf1cad7ab2533cf0609eb81fc92f54015
Denis H <XoMA32@yandex.ru>
2026-06-06 09:03:26 +0300

    commit ? 06/06

--- js-from-scratch-hw-js-main/lesson-03/03.js
+++ js-from-scratch-hw-js-main/lesson-03/03.js
@@ -18,27 +18,3 @@
 // findLargest(3, 3, 3);
 
 
-// const score = [
-//     60, 50, 60, 58, 54, 54,
-//     58, 50, 52, 54, 48, 69,
-//     34, 55, 51, 52, 44, 51,
-//     69, 64, 66, 55, 52, 61,
-//     46, 31, 57, 52, 44, 18,
-//     41, 53, 55, 61, 51, 44,
-// ];
-
-// let i = 0;
-// let output;
-// while (i < score.length) {
-//     output = "Bubble solution #" + " score: " + score[i];
-//     console.log(output);
-//     i = i + 1;
-// }
-//
-// console.log("Bubble tests: " + score.length);
-
-// for (let i = 0; i < score.length; i++) {
-//     output = "Bubble solution #" + " score: " + score[i];
-//     console.log(output);
-// };
-
---
+++ training/lesson-video/lesson-04_array/array_02.js
@@ -0,0 +1,64 @@
+
+// const scores = [
+//     60, 50, 60, 58, 54, 54,
+//     58, 50, 52, 54, 48, 69,
+//     34, 55, 51, 52, 44, 51,
+//     69, 64, 66, 55, 52, 61,
+//     46, 31, 57, 52, 44, 18,
+//     41, 53, 55, 61, 51, 44,
+// ];
+
+
+// while (i < score.length) {
+//     output = "Bubble solution #" + " score: " + score[i];
+//     console.log(output);
+//     i = i + 1;
+// }
+
+
+
+
+
+// let output;
+// let highScore = 0;
+//
+// for (let i = 0; i < scores.length; i++) {
+//     output = "Bubble solution #" + i +" score: " + scores[i];
+//     console.log(output);
+//     if(highScore < scores[i]){
+//         highScore = scores[i];
+//     }
+// }
+//
+// console.log("Bubble tests: " + scores.length);
+// console.log("Highscore bubble score: " + highScore);
+//
+// const bestSolutions = [];
+// for (let i = 0; i < scores.length; i++) {
+//     if (scores[i] === highScore){
+//         bestSolutions.push(i)
+//     }
+// }
+//
+// console.log("Solutions", bestSolutions)
+
+
+
+// function findAverage(array) {
+//     if (array.length === false){
+//         return 0;
+//     }else{
+//         let result = 0;
+//
+//         for (let i = 0; i > array.length ; i++) {
+//             result += array[i] ;
+//         }
+//
+//        return result/array.length
+//     }
+// }
+//
+// console.log(findAverage([1,2,3]));
+
+
+
