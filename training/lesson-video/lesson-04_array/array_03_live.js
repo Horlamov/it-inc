0c2d68d87086eebd3e5dee55fcffc80bbe6600a8
Denis H <XoMA32@yandex.ru>
2026-06-07 23:32:45 +0300

    create average, round

--- training/lesson-video/lesson-04_array/array_03_live.js
+++ training/lesson-video/lesson-04_array/array_03_live.js
@@ -1,11 +1,11 @@
-const array = [23, 56, 47, 30, 83, 98, 71];
+const array = [23, 56, 47, 30, 83, 98, 73];
 
 function getAverage(someArr) {
     let sum = 0;
     for (let i = 0; i < someArr.length; i++) {
         sum += someArr[i];
     }
-    return sum;
+    return Math.round(sum / someArr.length);
 }
 
 const average = getAverage(array);
