846c4d9f9ae3263ebee45885654bd6fe49caf8e2
Denis H <XoMA32@yandex.ru>
2026-06-10 20:47:20 +0300

    create converted array

--- training/lesson-video/lesson-04_array/array_03_live.js
+++ training/lesson-video/lesson-04_array/array_03_live.js
@@ -1,13 +1,20 @@
-const array = [23, 56, 47, 30, 83, 98, 73];
+const scores = [23, 56, 47, 30, 83, 98, 71, 92];
+const scores2 = [45, 26, 89, 34, 87, 47, 30, 83];
 
-function getAverage(someArr) {
-    let sum = 0;
-    for (let i = 0; i < someArr.length; i++) {
-        sum += someArr[i];
+function convertGrade(someScores){
+    let newScores = [];
+    for (let i = 0; i < someScores.length; i++) {
+        if (someScores[i] >= 80){
+            newScores.push(5);
+        } else if (someScores[i] >= 50) {
+            newScores.push(4);
+        } else if (someScores[i] >= 30){
+            newScores.push(3);
+        } else {
+            newScores.push(2);
+        }
     }
-    return Math.round(sum / someArr.length);
+    return newScores;
 }
-
-const average = getAverage(array);
-
-console.log(average);
+const convertedGrade = convertGrade(scores);
+console.log(convertedGrade);
