37360ba2c4fa4354e900b60af109562cd99cc8d2
Denis H <XoMA32@yandex.ru>
2026-06-15 21:37:01 +0300

    create remove array

--- training/lesson-video/lesson-04_array/array_04_live.js
+++ training/lesson-video/lesson-04_array/array_04_live.js
@@ -10,3 +10,20 @@ function reverseArray(someArr) {
 
 const reversedArray = reverseArray(superArray);
 console.log(reversedArray);
+
+function removeElement(someArr, element) {
+    const newArray = [];
+
+    for (let i = 0; i < someArr.length; i++) {
+        if (someArr[i] !== element) {
+            newArray.push(someArr[i])
+        }
+
+    }
+    return newArray
+}
+
+
+let filteredArray = removeElement(superArray,83)
+
+console.log(filteredArray)
