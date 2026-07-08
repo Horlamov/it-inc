689ef4566079b08355bcc7334c8f009531000a6e
Denis H <XoMA32@yandex.ru>
2026-07-08 19:21:22 +0300

    create homeWork array lesson_04 doubleEachCharacter

--- js-from-scratch-hw-js-main/lesson-04/04.js
+++ js-from-scratch-hw-js-main/lesson-04/04.js
@@ -1,5 +1,6 @@
 /*
-Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки повторяется дважды.
+Напишите функцию `doubleEachCharacter`, которая принимает строку и возвращает новую строку, где каждый символ из исходной строки
+повторяется дважды.
 
 Входные данные:
 
@@ -12,4 +13,16 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(string) {
+    let result = '';
+    for (let i = 0; i < string.length; i++) {
+        result += string[i] + string[i];
+
+    }
+    console.log(result)
+
+    return result
+}
+
+doubleEachCharacter('hello');
+
