936b1c960f5213eb2e47ae3a3f4f9b794e6fba7d
Denis H <XoMA32@yandex.ru>
2026-07-08 18:49:40 +0300

    create homeWork array lesson_03 findCommonElements

--- js-from-scratch-hw-js-main/lesson-04/03.js
+++ js-from-scratch-hw-js-main/lesson-04/03.js
@@ -17,4 +17,21 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(arr1, arr2) {
+    const result = [];
+    if (result === false) {
+        return result;
+    } else {
+    for (let i = 0; i < arr2.length; i++) {
+        if (includesElement(arr1, arr2[i]) === true) {
+            result.push(arr2[i]);
+        }
+        }
+    }
+    return result;
+}
+
+console.log(findCommonElements([1,2,2,3,4,4,5,7],[3,4,2,3,4,5,6,6,7]));
+console.log(findCommonElements(['apple', 'banana', 'apple', 'orange', 'banana'], ['apple', 'pumpkin', 'apple']));
+console.log(findCommonElements([1],[2]));
+
