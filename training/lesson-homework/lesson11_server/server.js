b651152c019edb0a04bcd4d0eeed68834fbc9277
Denis H <XoMA32@yandex.ru>
2026-08-18 10:03:37 +0300

    create homeWork lesson-server

--- training/lesson-homework/lesson11_server/server.js
+++ training/lesson-homework/lesson11_server/server.js
@@ -1,7 +1,26 @@
 const http = require('http');
 
+let requestCount = 0;
+
 const server = http.createServer((request, response) => {
-    response.write('it-Kamasutra');
+
+    requestCount++;
+
+    switch (request.url) {
+        case '/students':
+            response.write('students')
+            break;
+        case '/':
+        case '/courses':
+            response.write('FRONT + BACK')
+            break;
+        default:
+            response.write('404 not found')
+    }
+
+    response.write(' it-Kamasutra ' + requestCount);
     response.end();
 });
+
+
 server.listen(3003);
